5b0a39164e3f6c05652abf031878a2545a583f0e
Joseph Hughes <jdhughes99+1@gmail.com>
2025-09-04 18:22:27 -0500

    update mirror list

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+sed -i 's/mirror\.centos\.org/vault.centos.org/g' /etc/yum.repos.d/CentOS-*.repo
+sed -i 's/^#.*baseurl=http/baseurl=http/g' /etc/yum.repos.d/CentOS-*.repo
+sed -i 's/^mirrorlist=http/#mirrorlist=http/g' /etc/yum.repos.d/CentOS-*.repo
+
 yum update
 yum install -y git
 yum install -y gcc-gfortran
