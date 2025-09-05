53034446eac8da3fbbf5c85eb971ef4ef0507840
Joseph Hughes <jdhughes99+1@gmail.com>
2025-09-05 10:46:36 -0500

    get pip

--- install-deps.sh
+++ install-deps.sh
@@ -12,5 +12,10 @@ yum install -y centos-release-scl
 yum install -y devtoolset-10
 yum install -y ninja-build
 
+yum install -y python36
+yum install -y python36-devel
+yum install -y python36-setuptools
+easy_install-3.6 pip
+
 pip3 install meson
 meson --version
