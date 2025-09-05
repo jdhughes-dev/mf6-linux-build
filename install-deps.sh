6fa2010ef04135d09881e04a3a1ca8176826fd31
Joseph Hughes <jdhughes99+1@gmail.com>
2025-09-05 12:02:51 -0500

    get python 3.13 from python.org and build with gcc-10

--- install-deps.sh
+++ install-deps.sh
@@ -14,23 +14,19 @@ yum install -y devtoolset-10
 yum -y groupinstall "Development Tools"
 yum install -y openssl-devel bzip2-devel libffi-devel zlib-devel wget
 
-# source /opt/rh/devtoolset-10/enable
+source /opt/rh/devtoolset-10/enable
 
-# wget https://www.python.org/ftp/python/3.13.7/Python-3.13.7.tgz
-# tar xzf Python-3.13.7.tgz
-# cd Python-3.13.7
-# ./configure --enable-optimizations
-# make altinstall
+wget https://www.python.org/ftp/python/3.13.7/Python-3.13.7.tgz
+tar xzf Python-3.13.7.tgz
+cd Python-3.13.7
+./configure --enable-optimizations
+make altinstall
 
-# cd ..
-# sudo rm /usr/src/Python-3.13.7.tgz
-# python3.13 -V
+cd ..
+sudo rm /usr/src/Python-3.13.7.tgz
+python3.13 -V
 
-# python3.13 -m ensurepip --default-pip
-yum install python37
-yum install python37-devel
-yum install python37-setuptools
-easy_install-3.7 pip
+python3.13 -m ensurepip --default-pip
 
 pip3 install --user meson ninja
 meson --version
